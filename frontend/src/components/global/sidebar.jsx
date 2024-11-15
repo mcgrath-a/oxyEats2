c32f2c1d43a72fe2d69fc4193c4440a13ff0855e
alyanna <mcgratha@oxy.edu>
2024-11-15 13:44:38 -0800

    changing sidebar & student UX

--- frontend/src/components/global/sidebar.jsx
+++ frontend/src/components/global/sidebar.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState, useRef } from "react";
 import { IoCloseOutline } from "react-icons/io5";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import { FaEnvelopeOpenText, FaEnvelope } from "react-icons/fa";
-import { Button } from 'reactstrap';
-import { useDispatch, useSelector } from 'react-redux';
+import { Button } from "reactstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { setCurrentTab } from "../../store/sidebarTabsSlice";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function Sidebar({
   showSidebar,
@@ -15,11 +15,10 @@ export default function Sidebar({
   // handleSubscriptionToggle,
   // isSubscribed,
   tab,
-  setTab
+  setTab,
 }) {
-
   const navigate = useNavigate();
-  
+
   const dispatch = useDispatch();
   const credentials = useSelector((state) => state.credentials.credentials);
   const location = useLocation();
@@ -28,27 +27,43 @@ export default function Sidebar({
   // Create a ref to track the sidebar element
   const sidebarRef = useRef(null);
 
-  const menuItems = [{name: "Menu", route:"/"}, {name: "FAQ", route: "/faq"}];
-  const studentItems = ['Student Dashboard', "Favorite Items", "Newsletter", "Feedback Form", "User Preferences"];
-  const adminItems = ['Dashboard', 'Rating Insights', 'Favorite Insights', 'Banner Timing', "Menus"];
+  const menuItems = [
+    { name: "Menu", route: "/" },
+    { name: "FAQ", route: "/faq" },
+  ];
+  const studentItems = [
+    "Student Dashboard",
+    "Favorite Items",
+    "User Preferences",
+    "Newsletter",
+    "Feedback Form",
+  ];
+  const adminItems = [
+    "Dashboard",
+    "Rating Insights",
+    "Favorite Insights",
+    "Banner Timing",
+    "Menus",
+    "Add menu",
+  ];
 
   const currentTabHandler = (index) => {
     dispatch(setCurrentTab({ tab: index }));
     setTab(index);
-    handleSidebarToggle()
+    handleSidebarToggle();
   };
 
   // Effect to handle clicks outside the sidebar
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
-        handleSidebarToggle();  // Close sidebar if clicked outside
+        handleSidebarToggle(); // Close sidebar if clicked outside
       }
     };
 
     // Attach event listener to detect outside clicks
     document.addEventListener("mousedown", handleClickOutside);
-    
+
     return () => {
       // Clean up the event listener when the component is unmounted
       document.removeEventListener("mousedown", handleClickOutside);
@@ -56,41 +71,64 @@ export default function Sidebar({
   }, [handleSidebarToggle]);
 
   return (
-    <div ref={sidebarRef} style={{ width: '300px', height: '100vh', top: 0, zIndex: 10000 }} 
+    <div
+      ref={sidebarRef}
+      style={{ width: "300px", height: "100vh", top: 0, zIndex: 10000 }}
       className={`bg-lightorange sidebar position-fixed left-0 p-1 py-3  
-      ${showSidebar && !isAnimatingOut ? "animate__animated animate__slideInLeft animate__faster" : ""}
-      ${isAnimatingOut ? "animate__animated animate__slideOutLeft animate__faster" : ""}`}>
+      ${
+        showSidebar && !isAnimatingOut
+          ? "animate__animated animate__slideInLeft animate__faster"
+          : ""
+      }
+      ${
+        isAnimatingOut
+          ? "animate__animated animate__slideOutLeft animate__faster"
+          : ""
+      }`}
+    >
       <div className="d-flex flex-col align-items-center mx-3">
-        {path === '/' || path === '/faq' ? (
+        {path === "/" || path === "/faq" ? (
           <>
-            <div style={{ fontSize: '22px' }} 
-              className="my-1 mb-3 cursor-pointer w-100 d-flex justify-content-between text text-black border-circular">
+            <div
+              style={{ fontSize: "22px" }}
+              className="my-1 mb-3 cursor-pointer w-100 d-flex justify-content-between text text-black border-circular"
+            >
               <a>
                 <img width={60} height={60} src="img/logo.png" alt="" />
               </a>
               <IoCloseOutline onClick={handleSidebarToggle} />
             </div>
             {menuItems.map((item, index) => (
-              <div key={index} 
-                onClick={() => navigate(item.route)} 
-                className={`cursor-pointer w-100 p-2 ${tab === index ? 'bg-orange text-white' : 'text-black'} text border-circular`}>
+              <div
+                key={index}
+                onClick={() => navigate(item.route)}
+                className={`cursor-pointer w-100 p-2 ${
+                  tab === index ? "bg-orange text-white" : "text-black"
+                } text border-circular`}
+              >
                 ⦿ {item.name}
               </div>
             ))}
           </>
-        ):null}
-        {path === '/student' && (
+        ) : null}
+        {path === "/student" && (
           <>
-            <div style={{ fontSize: '22px' }} 
-              className="my-1 mb-3 cursor-pointer w-100 d-flex justify-content-between text text-black border-circular">
-                Hi, <strong>{credentials.fullName}</strong>
+            <div
+              style={{ fontSize: "22px", fontWeight: "bolder" }}
+              className="my-1 mb-3 cursor-pointer w-100 d-flex justify-content-between text text-black border-circular"
+            >
+              Hi, {credentials.fullName} 😊
               <IoCloseOutline onClick={handleSidebarToggle} />
             </div>
             {studentItems.map((item, index) => (
-              <div key={index} 
-                onClick={() => currentTabHandler(index)} 
-                style={{ fontWeight: "bold" }}
-                className={`cursor-pointer w-100 p-2 mb-2 ${tab === index ? 'bg-orange text-white' : 'text-black'} text border-circular`}>
+              <div
+                key={index}
+                onClick={() => currentTabHandler(index)}
+                style={{ fontWeight: "-moz-initial" }}
+                className={`cursor-pointer w-100 p-2 mb-2 ${
+                  tab === index ? "bg-orange text-white" : "text-black"
+                } text border-circular`}
+              >
                 ⦿ {item}
               </div>
             ))}
@@ -111,17 +149,23 @@ export default function Sidebar({
             )} */}
           </>
         )}
-        {path === '/admin' && (
+        {path === "/admin" && (
           <>
-            <div style={{ fontSize: '22px' }} 
-              className="my-1 mb-3 cursor-pointer w-100 d-flex justify-content-between text text-black border-circular">
+            <div
+              style={{ fontSize: "22px" }}
+              className="my-1 mb-3 cursor-pointer w-100 d-flex justify-content-between text text-black border-circular"
+            >
               Admin Dashboard
               <IoCloseOutline onClick={handleSidebarToggle} />
             </div>
             {adminItems.map((item, index) => (
-              <div key={index} 
-                onClick={() => currentTabHandler(index)} 
-                className={`cursor-pointer w-100 p-2 ${tab === index ? 'bg-orange text-white' : 'text-black'} text border-circular`}>
+              <div
+                key={index}
+                onClick={() => currentTabHandler(index)}
+                className={`cursor-pointer w-100 p-2 ${
+                  tab === index ? "bg-orange text-white" : "text-black"
+                } text border-circular`}
+              >
                 ⦿ {item}
               </div>
             ))}
